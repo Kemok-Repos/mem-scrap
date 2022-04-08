d38e04d4f0aa90b8534c384874d6e67ad9c912ab
Emilianni <emilianni@kemok.io>
2022-04-08 14:56:43 -0400

    Se agrego el llamado a la función para guardar los datos en BD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import pandas as pd
+import utils as u
+import Fdb as fdb
 
 URI = 'https://mem.gob.gt/que-hacemos/hidrocarburos/comercializacion-downstream/precios-combustible-nacionales/'
 
@@ -10,6 +12,17 @@ gas_price_table = gas_price_table.drop(gas_price_table.columns[[1, 3]], axis=1)
 last_week = gas_price_table.columns[1]
 last_week_curated = last_week.split(': ', 1)[1].replace('de', '')
 
+# Convierto la Fecha al formato dd/mm/yyyy
+last_week_curated = u.mesANumero(last_week_curated)
+
 gas_price_table.rename(columns={last_week : last_week_curated}, inplace=True)
 
-print(gas_price_table)
+#print(gas_price_table)
+
+# Obtengo los productos
+lProductos = list(gas_price_table[gas_price_table.columns[0]].values)
+# Obtengo los precios
+lPrecios = list(gas_price_table[gas_price_table.columns[1]].values)
+
+# Agrego los productos con su precio respectivo
+fdb.agregar(lProductos, lPrecios, gas_price_table.columns[1])
